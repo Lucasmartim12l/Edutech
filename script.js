ddb90bad9e99cca7c45ecdaa463a6913f75add5a
Lucasmartim12l <lucas.martim@eaportal.org>
2025-09-04 13:08:48 -0300

    carrossel de biomas

---
+++ script.js
@@ -0,0 +1,15 @@
+const slides = document.querySelector('.slides');
+const slideElements = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i) {
+  index = (i + slideElements.length) % slideElements.length;
+  slides.style.transform = `translateX(${-index * 600}px)`;
+}
+
+prev.addEventListener('click', () => showSlide(index - 1));
+next.addEventListener('click', () => showSlide(index + 1));
+
